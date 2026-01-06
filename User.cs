0247ed14e07af10922698d497ce4bb7609863f9f
Dimochca <dimae030409@gmail.com>
2026-01-06 19:09:14 +0700

    Create User.cs

---
+++ User.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmApp { 
+    class User
+    {
+        public string Login { get; set; }
+        public string Name { get; set; }
+        public bool IsPremium { get; set; }
+    }
+}
